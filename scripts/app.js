955bea74b948d0a1009a28b21acbdb636bf0612e
Jeff Mesens <jeff.mesens@gmail.com>
2016-04-18 12:13:10 +0200

    Use angular-material

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,92 @@
-var ScoreController = (function () {
-    function ScoreController() {
-        this.players = [{ "name": "Jeff" }];
-    }
-    return ScoreController;
-}());
-angular.module("Cricket", ['ngMaterial'])
-    .controller("ScoreController", ScoreController);
+var app;
+(function (app) {
+    var ScoreController = (function () {
+        function ScoreController($scope, $mdToast, $mdDialog) {
+            this.game = new ScoreBoard();
+            this.scope = $scope;
+            this.toast = $mdToast;
+            this.dialog = $mdDialog;
+        }
+        ScoreController.prototype.add = function (name) {
+            if (name == undefined)
+                return;
+            if (this.game.add(name)) {
+                this.scope.name = "";
+                this.showNotification("Player " + name + " added.");
+            }
+            else {
+                this.showNotification("Player " + name + " exists.");
+            }
+        };
+        ScoreController.prototype.showNotification = function (text) {
+            this.toast.show(this.toast.simple()
+                .position('top right')
+                .textContent(text)
+                .hideDelay(3000));
+        };
+        ScoreController.prototype.reset = function ($event) {
+            var _this = this;
+            var confirm = this.dialog.confirm()
+                .title('Reset game?')
+                .textContent('All players will be deleted and the points will be lost.')
+                .ariaLabel('Lucky day')
+                .targetEvent($event)
+                .ok('Reset')
+                .cancel('Cancel');
+            this.dialog.show(confirm).then(function () {
+                _this.game.initial();
+                _this.showNotification("Game is reset.");
+            });
+        };
+        return ScoreController;
+    }());
+    var ScoreBoard = (function () {
+        function ScoreBoard() {
+            this.initial();
+            this.add("Jeff");
+        }
+        ScoreBoard.prototype.initial = function () {
+            this.players = {};
+        };
+        ScoreBoard.prototype.registerHit = function (name, hit) {
+            this.get(name).registerHit(hit);
+        };
+        ScoreBoard.prototype.get = function (name) {
+            return this.players[name];
+        };
+        ScoreBoard.prototype.add = function (name) {
+            if (this.get(name) == null) {
+                this.players[name] = new Player(name);
+                return true;
+            }
+            return false;
+        };
+        return ScoreBoard;
+    }());
+    var Player = (function () {
+        function Player(name) {
+            this.name = name;
+            this.name = name;
+            this.initial();
+        }
+        Player.prototype.initial = function () {
+            this.hits = [];
+            this.points = 0;
+        };
+        Player.prototype.registerHit = function (hit) {
+            this.hits = this.hits.concat(hit);
+        };
+        return Player;
+    }());
+    // var player = new Player("Jeff");
+    // console.log(player);
+    // player.registerHit(20);
+    // console.log(player);
+    // var scoreBoard = new ScoreController();
+    // console.log(scoreBoard);
+    // console.log(scoreBoard.get("Jeff"));
+    // scoreBoard.registerHit("Jeff",19);
+    // console.log(scoreBoard.get("Jeff"));
+    angular.module("Cricket", ['ngMaterial'])
+        .controller("ScoreController", ScoreController);
+})(app || (app = {}));
--- scripts/app.ts
+++ scripts/app.ts
@@ -1,18 +1,122 @@
-interface IScoreModel{
-    players: any[];
+module app{
     
-}
-
-class ScoreController implements IScoreModel{
-    players: any[];
-    
-    constructor(){
-        this.players = [{"name":"Jeff"}]
+    interface IScope extends ng.IScope{
+        name: string;
+        players: { [name: string]: Player; };
+        reset(): void;
     }
     
+    class ScoreController{
+        game = new ScoreBoard();
+        scope: IScope;
+        toast: angular.material.IToastService;
+        dialog: angular.material.IDialogService;
+        
+        constructor($scope: IScope, $mdToast: angular.material.IToastService, $mdDialog: angular.material.IDialogService){
+            this.scope = $scope;
+            this.toast = $mdToast;
+            this.dialog = $mdDialog;
+        }
+        add(name:string){
+            if(name==undefined)
+                return;
+            if(this.game.add(name)){
+                this.scope.name="";
+                this.showNotification("Player " + name + " added.");
+            }else{
+                this.showNotification("Player " + name + " exists.");
+            }
+        }
+        showNotification(text: string){
+            this.toast.show(
+                this.toast.simple()
+                    .position('top right')
+                    .textContent(text)
+                    .hideDelay(3000)
+            );
+        }
+        reset($event: MouseEvent){
+            var confirm = this.dialog.confirm()
+                .title('Reset game?')
+                .textContent('All players will be deleted and the points will be lost.')
+                .ariaLabel('Lucky day')
+                .targetEvent($event)
+                .ok('Reset')
+                .cancel('Cancel');
+            this.dialog.show(confirm).then(
+                () => {
+                    this.game.initial();
+                    this.showNotification("Game is reset.");
+            });
+        }
+            
+
+    }
+
+    interface IScoreBoard{
+        players: { [name: string]: Player; };
+        registerHit(Player, number): void;
+        initial(): void;
+        get(name: string): Player;
+        add(name: string): boolean;
+        
+    }
+
+    class ScoreBoard implements IScoreBoard{
+        players:  { [name: string]: Player; };
+        
+        constructor(){
+            this.initial();
+            this.add("Jeff");
+        }
+        initial(): void{
+            this.players = {};
+        }
+        registerHit(name: string, hit: number): void{
+            this.get(name).registerHit(hit);
+        }
+        get(name:string){
+            return this.players[name];
+        }
+        add(name:string){
+            if(this.get(name)==null){
+                this.players[name] = new Player(name);
+                return true;
+            }
+            return false;
+            
+        }
+        
+    }
+
+    class Player{
+        hits: any[];
+        points: number;
+        
+        constructor(public name: string){
+            this.name=name;
+            this.initial();
+        }
+        initial(){
+            this.hits = [];
+            this.points = 0;
+        }
+        registerHit(hit: number){
+            this.hits = this.hits.concat(hit);
+        }
+    }
+
+    // var player = new Player("Jeff");
+    // console.log(player);
+    // player.registerHit(20);
+    // console.log(player);
+
+    // var scoreBoard = new ScoreController();
+    // console.log(scoreBoard);
+    // console.log(scoreBoard.get("Jeff"));
+    // scoreBoard.registerHit("Jeff",19);
+    // console.log(scoreBoard.get("Jeff"));
+
+    angular.module("Cricket", [ 'ngMaterial' ] )
+        .controller("ScoreController",  ScoreController);
 }
-
-
-angular.module("Cricket", [ 'ngMaterial' ] )
-    .controller("ScoreController",  ScoreController);
-    
